cd47d2fa180a145f9568a5351877d8f976be6282
fawkej <james.fawke@g4pas.co.uk>
2017-02-14 08:14:20 +0000

    Added Error Channel

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mvn clean install
-
-java -jar target/dp-publish-pipeline-enrich-search.jar -Xmx2gb -Xms2gb
+export S3_SECRET_ACCESS_KEY=II/8TmtrxZ/YenymB6T211KgCvx1oS+pm3bHJ1US
+export S3_ACCESS_KEY=AKIAIX65FEZKPY3H6AHQ
+target/dp-publish-pipeline-enrich-search.jar start
