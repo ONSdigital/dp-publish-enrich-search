621c73aae8efd5327629545f6c5afc872e92c49e
fawkej <james.fawke@g4pas.co.uk>
2017-02-20 11:22:22 +0000

    Updated all references to inbound URL field to FileLocation as this has appeared to have been missed

--- src/main/java/com/github/onsdigital/index/enrichment/service/NullReturningTerminatingService.java
+++ src/main/java/com/github/onsdigital/index/enrichment/service/NullReturningTerminatingService.java
@@ -3,8 +3,26 @@ package com.github.onsdigital.index.enrichment.service;
 import org.springframework.stereotype.Service;
 
 /**
- * Created by fawks on 16/02/2017.
- */
+ *
+ * The Integration flows wait for response of a gateway, if no response appears it just hangs.
+ *  If you return something from the gateway then the calling flow requires a responseChannel, but we don want to respond to Kafka so we need
+ *  to consume the response and end the flow here.<p/>
+ *
+ *  You can see this in the <code>ConsumerDefinition</code><p/>
+ *
+ * @see com.github.onsdigital.index.enrichment.ConsumerDefinition
+ *
+ *  <code>
+ *  return IntegrationFlows<br/>
+ *  .from(messageConsumer)<br/>
+ *  .handle(transformService)<br/>
+ *  .handle(extractService)<br/>
+ *  .gateway(processChannel(),<br/>
+ *  e -> e.advice(retryAdvice()))//Call Gateway flow and then retry if any exception<br/>
+ *  .handle(terminatingService)// Fix the integration flow issue to return to the queue and pick up another, otherwise it just waits<br/>
+ *  .get();<br/>
+ *  </code>
+ **/
 @Service
 public class NullReturningTerminatingService {
     public void doNothingReturnNothing(Object object) {
